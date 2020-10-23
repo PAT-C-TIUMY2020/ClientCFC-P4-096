59aed8610c7ed89e05b091896611053f85a651f8
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2020-10-23 23:13:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ServiceMtk_P2_096;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCFC_P4_096
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:8080");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+
+            Koordinat objk = new Koordinat();
+        }
+    }
+}
